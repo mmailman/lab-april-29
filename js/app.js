268d848ab2e4027f8b2df17b6aa882a4fd563a94
Michael Mailman <mmailman@gmail.com>
2016-04-29 15:11:02 -0700

    Adding output display and submit handler.

--- js/app.js
+++ js/app.js
@@ -45,3 +45,52 @@ function tonsCon(type, value){
     return value / 0.907185;
   }
 }
+
+//Submit Handler
+function submitHandler(event){
+  event.preventDefault();
+  var dropDownValue = document.getElementById('dropdown').value;
+  var convertedValue = 0;
+  console.log(dropDownValue);
+  switch (dropDownValue){
+  case 'inches':
+    convertedValue = inchesCon('standard', parseFloat(event.target.rawValue.value));
+    break;
+  case 'cm':
+    convertedValue = inchesCon('metric', parseFloat(event.target.rawValue.value));
+    break;
+  case 'flOz':
+    convertedValue = flOuncesCon('standard', parseFloat(event.target.rawValue.value));
+    break;
+  case 'ml':
+    convertedValue = flOuncesCon('metric', parseFloat(event.target.rawValue.value));
+    break;
+  case 'miles':
+    convertedValue = milesCon('standard', parseFloat(event.target.rawValue.value));
+    break;
+  case 'km':
+    convertedValue = milesCon('metric', parseFloat(event.target.rawValue.value));
+    break;
+  case 'lbs':
+    convertedValue = lbsCon('standard', parseFloat(event.target.rawValue.value));
+    break;
+  case 'kg':
+    convertedValue = lbsCon('metric', parseFloat(event.target.rawValue.value));
+    break;
+  case 'tons':
+    convertedValue = tonsCon('standard', parseFloat(event.target.rawValue.value));
+    break;
+  case 'mTons':
+    convertedValue = tonsCon('metric', parseFloat(event.target.rawValue.value));
+    break;
+  }
+  convertedValue = convertedValue.toFixed(3);
+  console.log(convertedValue);
+
+  var resultsDisplay = document.getElementById('results');
+  resultsDisplay.value = convertedValue;
+}
+
+//Event Listener
+var conversionForm = document.getElementById('conversionForm');
+conversionForm.addEventListener('submit', submitHandler);
